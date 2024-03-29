9b3cfd183ee4b81ad8c141e769ba8b8a9b6dd42e
Sezynex <100211433+Sezynex@users.noreply.github.com>
2024-03-29 11:09:58 +0300

    Update save.py

--- save.py
+++ save.py
@@ -1,10 +1,9 @@
 import cv2
 import argparse
-
+import time
 from ultralytics import YOLO
 import supervision as sv
 import numpy as np
-
 
 ZONE_POLYGON = np.array([
     [0, 0],
@@ -23,18 +22,22 @@ def main():
     )
 
     model = YOLO("./yaprak.pt")
+    reset_counter = 0
+    reset_threshold = 10  # Sıfırlama eşiğini belirle (örneğin, 10)
+    
     for result in model.track(source=0, show=True, stream=True, agnostic_nms=True):
         
         frame = result.orig_img
         detections = sv.Detections.from_yolov8(result)
 
         if result.boxes.id is not None:
-            detections.tracker_id = result.boxes.id.cpu().numpy().astype(int)
+            detected_ids = "-".join(str(x) for x in result.boxes.id.cpu().numpy().astype(int))
+            print("Algılanan nesnelerin idleri:", detected_ids)
         
         detections = detections[(detections.class_id != 60) & (detections.class_id != 0)]
 
         labels = [
-            f"{tracker_id} {model.model.names[class_id]} {confidence:0.2f}"
+            f"{tracker_id} {model.model.names[class_id]} {confidence:0.4f}"
             for _, confidence, class_id, tracker_id
             in detections
         ]
@@ -45,11 +48,17 @@ def main():
             labels=labels
         )
 
-        cv2.imshow("yolov8", frame)
+        # Her bir nesne için ID'yi kontrol et
+        for _, _, _, tracker_id in detections:
+            if tracker_id == 10:  # Eğer 10. ID'ye ulaşılırsa
+                reset_counter += 1
+                if reset_counter == reset_threshold:  # Sıfırlama eşiğine ulaşıldığında
+                    detections.tracker_id = None
+                    reset_counter = 0  # Sayaç sıfırlanır
+
 
         if (cv2.waitKey(30) == 27):
             break
-
 
 if __name__ == "__main__":
     main()
